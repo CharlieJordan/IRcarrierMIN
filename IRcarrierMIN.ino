c70ecded96e127a190dd6dd09f9ec708593ce4df
CharlieJordan <charlie@cjordan.net>
2012-07-26 10:43:38 -0500

    changed <myIRh> to "myIR.h"

--- IRcarrierMIN.ino
+++ IRcarrierMIN.ino
@@ -2,10 +2,11 @@
 // IRcarrierMIN
 // 
 // Generate a 38 MHz PWM signal 
+//                last modified - 26 July 2012, CAJ
 
 
 #include <TimerOne.h>
-#include <myIR.h>
+#include "myIR.h"
 
 const byte IRtransmitPin = 9;  // Primary fire mode IR transmitter pin
 const byte period = 26;        // 1/freq : e.g: for 38000Hz, period = 26
